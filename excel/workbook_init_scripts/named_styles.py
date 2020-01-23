54e4044434ef808a58e6318d06cd0d801ac2a07c
Nathan S <nscstevenson@mun.ca>
2020-01-23 16:57:26 -0700

    Removed some white space.

--- excel/workbook_init_scripts/named_styles.py
+++ excel/workbook_init_scripts/named_styles.py
@@ -3,7 +3,6 @@ from openpyxl.styles import NamedStyle, Alignment, Font, PatternFill, Color, fil
 from workbook_init_scripts.border_presets import f_label_border, title_border
 
 
-
 # --------------- Title header styles ---------------
 # Balance header
 title_style_b = NamedStyle(name='balance_title')
@@ -23,14 +22,18 @@ title_style_e.border = title_border
 title_style_i = NamedStyle(name='income_title')
 title_style_i.font = Font(size=20, bold=True, underline='single', )
 title_style_i.alignment = Alignment(horizontal='center', vertical='center')
-title_style_i.fill = PatternFill(fgColor=Color("66CC66"), fill_type=fills.FILL_SOLID)
+title_style_i.fill = PatternFill(fgColor=Color("66CC66"),
+                                 fill_type=fills.FILL_SOLID
+                                 )
 title_style_i.border = title_border
 
 # Control header
 title_style_c = NamedStyle(name='control_title')
 title_style_c.font = Font(size=20, bold=True, underline='single', )
 title_style_c.alignment = Alignment(horizontal='center', vertical='center')
-title_style_c.fill = PatternFill(fgColor=Color("AA00FF"), fill_type=fills.FILL_SOLID)
+title_style_c.fill = PatternFill(fgColor=Color("AA00FF"),
+                                 fill_type=fills.FILL_SOLID
+                                 )
 title_style_c.border = title_border
 
 
@@ -46,4 +49,3 @@ ph_style = NamedStyle(name="ph_style")
 ph_style.alignment = Alignment(horizontal='center', vertical='center')
 ph_style.border = f_label_border
 ph_style.number_format = '$#,##0.00'
-
